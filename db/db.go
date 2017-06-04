a09e1ffc5191d568ce308e8e7555f1af0c1ce172
Sadayuki Matsuno <sadayuki.matsuno@gmail.com>
2017-06-05 05:20:14 +0900

    add redis

--- db/db.go
+++ db/db.go
@@ -2,35 +2,96 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
 
-	"../models"
+	"github.com/go-redis/redis"
 	"github.com/inconshreveable/log15"
 	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"github.com/vattle/sqlboiler/boil"
 )
 
-func init() {
-	db, err := sql.Open("postgres", `host=0.0.0.0  user=postgres dbname=saas sslmode=disable password=vuls`)
-	if err != nil {
-		log15.Error("Failed to open database", "err", err)
+var redisClient *redis.Client
 
-	}
-	log15.Info("connectd to db")
-	boil.SetDB(db)
+func init() {
 }
 
-// SelectSome : select
-func SelectSome() {
-	cveDetail, err := models.CveDetailsG().One()
-	if err != nil {
-		log15.Error("Failed to Read cveDetail", "err", err)
+// OpenDBs : OpenDBs
+func OpenDBs() (err error) {
+	if err = prepareViper(); err != nil {
+		log15.Warn("Failed to read viper config file", "method", "main.main", "err", err)
+		return err
 	}
-	log15.Info("Fetched cveDetail", "CVEID", cveDetail.CveID)
+	var postgresURL = fmt.Sprintf(
+		"host=%s user=%s dbname=%s sslmode=%s password=%s",
+		viper.GetString("postgres.host"),
+		viper.GetString("postgres.user"),
+		viper.GetString("postgres.dbname"),
+		viper.GetString("postgres.sslmode"),
+		viper.GetString("postgres.pass"),
+	)
 
-	cpe, err := models.CpesG().One()
-	if err != nil {
-		log15.Error("Failed to Read cpe", "err", err)
+	if err = connectPostgres(postgresURL); err != nil {
+		log15.Error("errmsg", "method", "db.OpenDBs", "err", err)
+		return err
 	}
-	log15.Info("Fetched cpe", "cpe", cpe)
+	if err = connectRedis(viper.GetString("redis.url")); err != nil {
+		log15.Error("errmsg", "method", "db.OpenDBs", "err", err)
+		return err
+	}
+	return nil
+}
 
+func connectRedis(url string) error {
+	var err error
+	var option *redis.Options
+	if option, err = redis.ParseURL(url); err != nil {
+		log15.Error("Failed to open redis", "url", url, "err", err)
+		return err
+	}
+	if redisClient = redis.NewClient(option); redisClient.Ping().Err() != nil {
+		return redisClient.Ping().Err()
+	}
+	log15.Info("connected to redis ", "url", url)
+	return nil
+}
+
+func connectPostgres(url string) error {
+	postgres, err := sql.Open("postgres", url)
+	if err != nil {
+		log15.Error("Failed to open postgres", "url", url, "err", err)
+		return err
+	}
+
+	boil.SetDB(postgres)
+	log15.Info("connected to postgres", "url", url)
+	return nil
+}
+
+func prepareViper() error {
+	log15.Info("call prepareViper", "method", "main.prepareViper")
+	viper.SetConfigName("sqlboiler")
+
+	configHome := os.Getenv("XDG_CONFIG_HOME")
+	homePath := os.Getenv("HOME")
+	wd, err := os.Getwd()
+	if err != nil {
+		wd = "./"
+	}
+
+	configPaths := []string{wd}
+	if len(configHome) > 0 {
+		configPaths = append(configPaths, filepath.Join(configHome, "sqlboiler"))
+	} else {
+		configPaths = append(configPaths, filepath.Join(homePath, ".config/sqlboiler"))
+	}
+
+	for _, p := range configPaths {
+		viper.AddConfigPath(p)
+	}
+
+	err = viper.ReadInConfig()
+	return err
 }
